67d9d6455348d243d430702e7ad7ca1c8531e42d
Vinh Phạm <pvinhit75@gmail.com>
2023-05-14 15:18:57 +0700

    Add project files.

---
+++ ProjectPersional.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectPersional.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
