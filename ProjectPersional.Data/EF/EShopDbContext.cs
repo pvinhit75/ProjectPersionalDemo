bd016dc43a6c74a51ee942fbcddfa0ab578a3e4e
Vinh Phạm <pvinhit75@gmail.com>
2023-05-21 13:34:29 +0700

    ASP .NET Core API

---
+++ ProjectPersional.Data/EF/EShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectPersional.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPersional.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
